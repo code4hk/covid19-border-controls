ba4fe707cda4e3d3f3a799d1105792f67df307c4
Vincent Lau <lauchunyin@gmail.com>
2020-03-17 12:55:54 +0800

    support global countryCode

--- messages/build.test.js
+++ messages/build.test.js
@@ -7,15 +7,32 @@ import path from 'path'
 import { defaultLocales } from '../i18n'
 describe('build locale json', () => {
   test('build', async () => {
+    const overrideByLocale = {
+      'zh-TW': {
+        'country.hk': '香港',
+        'country.mo': '澳門',
+        'country.global': '全球'
+      },
+      en: {
+        'country.global': 'Global'
+      },
+      ja: {
+        'country.global': '世界的'
+      },
+      ko: {
+        'country.global': '글로벌'
+      }
+    }
     defaultLocales.forEach(
       async locale => {
         let dataLocale = locale
-        const override = {}
+
         if (locale === 'zh-TW') {
           dataLocale = 'zh_Hant_TW'
           // this is better
-          override['country.hk'] = '香港'
-          override['country.mo'] = '澳門'
+          // override['country.hk'] = '香港'
+          // override['country.mo'] = '澳門'
+          // override['country.global'] = '全球'
         }
         const countries = await fetch('https://raw.githubusercontent.com/umpirsky/country-list/master/data/' + dataLocale + '/country.json')
         // const languagesNative = await fetch('https://raw.githubusercontent.com/umpirsky/language-list/master/data/' + dataLocale + '/language.json')
@@ -23,7 +40,7 @@ describe('build locale json', () => {
           .then(res => res.json())
         const data = {
           ..._.mapKeys(countries, (v, k) => 'country.' + _.toLower(k)),
-          ...override,
+          ...overrideByLocale[locale],
           'language.native.zh-TW': '中文',
           'language.native.en': 'English',
           'language.native.ja': '日本語',
